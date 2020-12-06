1537e204e6a03bffecc983d1a42f11eb15033211
Michael Dinh <tonkoalsce@gmail.com>
2020-12-06 19:07:34 +1100

    Basic outline of questions done

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var opt1: UIButton!
+    @IBOutlet weak var opt2: UIButton!
+    @IBOutlet weak var opt3: UIButton!
+    @IBOutlet weak var opt4: UIButton!
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var correctAnswers : Int = 0;
+    var questionNumber : Int = 0;
+    var questions = QuestionPack();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
